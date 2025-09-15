872d5d2df83a973b6389d4ed5e441314c1efa5cf
hadia-glitch <hadianoorikram@gmail.com>
2025-09-15 10:58:15 +0500

    Task7 added

---
+++ task7.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isempty(int arr[], int n) {
+    return n == 0; 
+}
+
+vector<int> findIndexes(int arr[], int n, int target) {
+    vector<int> indexes;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            indexes.push_back(i); 
+        }
+    }
+    return indexes;
+}
+
+int main() {
+    int numbers[6] = {1, 2, 3, 2, 5, 2};
+    int n = 6;  
+    
+    int target;
+    cout << "Enter element to be found: ";
+    cin >> target;
+    
+    vector<int> result = findIndexes(numbers, n, target);
+    
+    if (isempty(numbers, n)) {
+        cout << "Array is empty";
+    } else if (result.empty()) {
+        cout << "Element not found";
+    } else {
+        cout << "Element found at indexes: ";
+        for (int i = 0; i < result.size(); i++) {
+            cout << result[i] << " ";
+        }
+    }
+    
+    return 0;
+}
